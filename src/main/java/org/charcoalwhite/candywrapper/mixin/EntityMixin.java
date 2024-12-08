d33b1a31b4ff339d46fee132275dbebf6cd4d197
CharcoalWhite <125453632+CharcoalWhite@users.noreply.github.com>
2024-12-08 15:29:28 +0800

    Fix not being able to ride players for 1.21.3.

--- src/main/java/org/charcoalwhite/candywrapper/mixin/EntityMixin.java
+++ src/main/java/org/charcoalwhite/candywrapper/mixin/EntityMixin.java
@@ -2,6 +2,7 @@ package org.charcoalwhite.candywrapper.mixin;
 
 import java.util.Set;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.network.packet.s2c.play.EntityPassengersSetS2CPacket;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.charcoalwhite.candywrapper.api.EntityApi;
@@ -9,6 +10,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Entity.class)
@@ -16,6 +18,21 @@ public abstract class EntityMixin implements EntityApi{
 	@Override
     public boolean hasCommandTag(String commandTag) {
         return this.getCommandTags().contains(commandTag);
+    }
+
+    @Redirect(
+        method = "startRiding(Lnet/minecraft/entity/Entity;Z)Z",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/entity/EntityType;isSaveable()Z"
+        )
+    )
+    private boolean isSaveableOrPlayer(EntityType<?> entityType) {
+        if (entityType.isSaveable()) {
+            return true;
+        }
+
+        return this.isPlayer();
     }
 
     @Inject(
