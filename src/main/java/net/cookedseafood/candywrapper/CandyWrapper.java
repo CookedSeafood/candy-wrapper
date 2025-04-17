cbc221ec2deb6bd886043a427a19ab86ac09f1cd
CookedSeafood <125453632+CookedSeafood@users.noreply.github.com>
2025-04-17 13:19:07 +0800

    Update to 0.5.10.

--- src/main/java/net/cookedseafood/candywrapper/CandyWrapper.java
+++ src/main/java/net/cookedseafood/candywrapper/CandyWrapper.java
@@ -16,7 +16,7 @@ public class CandyWrapper implements ModInitializer {
 
 	public static final byte VERSION_MAJOR = 0;
 	public static final byte VERSION_MINOR = 5;
-	public static final byte VERSION_PATCH = 9;
+	public static final byte VERSION_PATCH = 10;
 
 	@Override
 	public void onInitialize() {
